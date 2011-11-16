d789dfb58686c09106c258158ab657b7fb0fe42a
Olexandr Savchuk <mail@olex.biz>
2011-11-16 22:15:33 +0100

    Version update

--- version.php
+++ version.php
@@ -5,7 +5,7 @@
 //  This fragment is called by /admin/index.php
 ////////////////////////////////////////////////////////////////////////////////
 
-$module->version  = 2011102601;
+$module->version  = 2011111600;
 $module->requires = 2010080300;  // Requires this Moodle version
 $module->cron     = 0;
 
