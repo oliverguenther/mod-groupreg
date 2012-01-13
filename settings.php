3415a7d8d9d7b4c37704ea583a5ac8d359345669
Oliver Guenther <mail@oliverguenther.de>
2012-01-13 17:08:12 +0100

    settings_perltime_desc lang fixed

--- settings.php
+++ settings.php
@@ -4,6 +4,6 @@ defined('MOODLE_INTERNAL') || die;
 
 if ($ADMIN->fulltree) {
 
-    $settings->add(new admin_setting_configtext('groupreg_perltime', get_string('settings_perltime', 'groupreg'), get_string('settings_perlscript_desc', 'groupreg'), 30, PARAM_INT));
+    $settings->add(new admin_setting_configtext('groupreg_perltime', get_string('settings_perltime', 'groupreg'), get_string('settings_perltime_desc', 'groupreg'), 30, PARAM_INT));
     
 }
